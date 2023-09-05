04f27400549273dfac5c240a84722e202d6929ee
lohithsrk <srklohith05@gmail.com>
2023-09-05 21:55:43 +0530

    pro

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 git add .
 git commit -m $1
 git push origin master
-gh pr create --head master --base prod
+# gh pr create --head master --base prod
+
+npm run build
+firebase deploy
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -11,7 +11,6 @@ const Home = () => {
 		<div className='overflow-x-hidden'>
 			<Banner />
 			<Services />
-			
 			<Achievements />
 			<Projects />
 			<Testimonials />
